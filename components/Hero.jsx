11173c8dc25388db63bd0e12d332d2bcf547869d
Prashmin Gurung <prashmingrg@gmail.com>
2024-05-21 15:20:03 +1000

    hero section is in progress

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,31 +1,61 @@
 import React from "react";
 
 import Image from "next/image";
+import Link from "next/link";
+import { BsArrowRight } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
 
-import profilePic from "../public/prasmin_profile.png";
+import me from "../public/Me.jpg";
 
 export default function Hero() {
   return (
-    <div className=" flex text-left items-center justify-center  mt-5">
-      <article className=" max-w-screen-md   text-wrap">
-        <h3 className="  text-3xl font-black">
-          Fullstack Developer: Experienced Innovator Delivering Value to Your
-          Team.
-        </h3>
-        <p className="font-normal text-gray-500 tracking-wide py-6 text-2xl ">
-          Hi! I'm Prashmin. A seasoned full-stack developer with a passion for
-          turning ideas into polished, high-performance solutions.
-        </p>
-      </article>
+    <>
+      <div className="flex flex-col sm:flex-row items-center mx-auto justify-center gap-8 p-4">
+        <div className="flex-1 ">
+          <article>
+            <h2 className=" text-3xl font-bold tracking-normal leading-normal sm:leading-normal  sm:text-4xl ">
+              Fullstack Developer, Balanced Life Coder, Multicultural
+              Collaborator.
+            </h2>
+            <p className=" text-gray-500 leading-8 tracking-wide mt-6 text-xl  ">
+              Hi! I'm Prashmin. A seasoned full-stack developer with a passion
+              for turning ideas into polished, high-performance solutions.
+            </p>
+          </article>
 
-      <Image
-        src={profilePic}
-        alt="prasmin"
-        // style={{
-        //   maxWidth: "100%",
-        //   height: "auto",
-        // }}
-      />
-    </div>
+          <div className="flex flex-col mt-3 sm:flex-row items-center  gap-2  text-lg font-medium">
+            <Link
+              href="#contact"
+              className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full  "
+            >
+              Contact me here <BsArrowRight />
+            </Link>
+            <a className="bg-white px-7 py-3   flex items-center gap-2 rounded-full">
+              Download CV <HiDownload />
+            </a>
+            <Link
+              href="https://www.linkedin.com/in/prasmin-gurung/"
+              className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full  "
+            >
+              <BsLinkedin />
+            </Link>
+            <Link
+              href="https://github.com/Prasmin"
+              className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full  "
+            >
+              <FaGithubSquare />
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex-1 flex items-center justify-center ">
+          <Image src={me} alt="prasmin" />
+        </div>
+      </div>
+    </>
   );
 }
+
+// mx-auto  text-center  lg:flex lg:py-32 lg:text-left
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,27 +1,76 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import cn from "@/lib/utils";
+
 import Link from "next/link";
+import Image from "next/image";
+
+import profilePic from "../public/prasmin_profile.png";
 
 const navigation = [
   { name: "Dashboard", href: "/", current: true },
   { name: "Team", href: "/team", current: false },
   { name: "Projects", href: "/project", current: false },
   { name: "Uses", href: "/use", current: false },
+  { name: "Contact", href: "/contact", current: false },
 ];
 
-//if i use like object instead array. would i still map methods to sort out the array?
+//if i use like object instead ar ray. would i still map methods to sort out the array?
 
 const Navbar = () => {
+  // Function to close the mobile navbar
+  const closeMobileNavbar = () => {
+    setIsMobileNavOpen(false);
+  };
+
+  // Effect to listen for screen size changes
+  useEffect(() => {
+    // Function to handle resize event
+    const handleResize = () => {
+      // Close the mobile navbar if the screen size is larger than a certain threshold
+      if (window.innerWidth > 768) {
+        closeMobileNavbar();
+      }
+    };
+
+    // Add event listener for resize event
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup function to remove event listener
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
   return (
-    <header className=" flex justify-center  ">
-      <div className="max-w-md shadow-xl border-white   rounded-full">
-        <nav className=" p-4 m-4 ">
+    <header className=" flex justify-around pt-3 lg:justify-evenly items-center">
+      <div className="  ">
+        <Link href="/">
+          <span className="sr-only">Prashmin Gurung</span>
+          <Image
+            width="192"
+            height="192"
+            className="h-12 w-12 rounded-full object-cover bg-top  saturate-200 border-white shadow-xl"
+            quality="100"
+            src={profilePic}
+            alt="logo"
+          />
+        </Link>
+      </div>
+
+      <div className="  shadow-xl border-white rounded-full   ">
+        {/* Desktop Navigation */}
+        <nav className=" hidden lg:flex p-4 m-4 ">
           {navigation.map((item) => (
             <Link
               key={item.name}
               href={item.href}
               className={cn("rounded-md px-3 py-2 text-sm font-bold ", {
                 // Apply styles for the current navigation item
-                "  hover:text-slate-500 ": item.current,
+                "  hover:text-slate-500": item.current,
                 // Apply styles for non-current navigation items
                 "text-slate-950 hover:text-slate-500 ": !item.current,
               })}
@@ -31,6 +80,58 @@ const Navbar = () => {
             </Link>
           ))}
         </nav>
+
+        {/* Mobile Navigation */}
+        <div className="lg:hidden ">
+          <button
+            className="block text-gray-900 hover:text-gray-500 focus:outline-none focus:text-gray-500"
+            onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
+          >
+            <svg
+              className="h-6 w-6 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {isMobileNavOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              )}
+            </svg>
+          </button>
+
+          {/* Mobile Navigation Menu */}
+          {isMobileNavOpen && (
+            <div className="absolute top-16 right-0  backdrop-blur-sm bg-white/70 w-full   shadow-xl border rounded-md">
+              <nav className="flex flex-col p-4">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className={cn("px-4 py-2 text-xl font-bold", {
+                      "hover:text-slate-500": item.current,
+                      "text-slate-950 hover:text-slate-500": !item.current,
+                    })}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </nav>
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
